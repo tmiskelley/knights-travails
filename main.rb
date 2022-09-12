af532dfaec4879d601a160071505166418be98fd
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-09-12 13:18:18 -0400

    Created "Square" class

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+# Represents individual square on chess board
+class Square
+  MOVES = [
+    [1, 2], [-2, -1], [-1, 2], [2, -1],
+    [1, -2], [-2, 1], [-1, -2], [2, 1]
+  ].freeze
+
+  def initialize(data)
+    @position = data
+    @children = add_children(@position)
+  end
+
+  def add_children(coordinate)
+    children = MOVES.map { |arr| [coordinate[0] + arr[0], coordinate[1] + arr[1]] }
+    children.reject! { |arr| arr.reject!(&:negative?) }
+    children
+  end
+end
