5f4dfed35651993d93c46950fade6c139d88e09f
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-09-26 12:43:32 -0400

    Refactored "Square" and "GameBoard" classes

--- main.rb
+++ main.rb
@@ -1,48 +1,54 @@
 # frozen_string_literal: true
 
-# Represents individual square on chess board
+# Models an individual sqaure on chess board
 class Square
-  attr_reader :position, :children
+  attr_reader :value, :parent, :children
 
   MOVES = [
     [-2, -1], [-2, 1], [-1, -2], [-1, 2],
     [1, -2], [1, 2], [2, -1], [2, 1]
   ].freeze
 
-  def initialize(data)
-    @position = data
-    @children = add_children
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = create_children
   end
 
-  def add_children
-    children = MOVES.map { |arr| [@position[0] + arr[0], @position[1] + arr[1]] }
+  private
+
+  def create_children
+    children = MOVES.map { |arr| [@value[0] + arr[0], @value[1] + arr[1]] }
     children.reject! { |arr| arr.reject! { |e| e > 7 || e.negative? } }
     children
   end
 end
 
-# Represents an 8x8 chess game board, comprised of 64 square nodes
+# Models a chess game board, comprised of 64 square nodes
 class GameBoard
   attr_reader :squares
 
   def initialize
-    @squares = []
-    create_squares
+    @squares = create_squares
   end
 
-  def create_squares
-    coordinates = [*0..7].repeated_permutation(2).to_a
-    coordinates.each { |arr| @squares.push(Square.new(arr)) }
-  end
-
-  def find(coordinate)
-    @squares.each do |square|
-      return square if square.position == coordinate
-    end
-    raise "Square #{coordinate} could not be found."
+  def find_square(value)
+    @squares.each { |square| return square if square.value == value }
+    raise "Square #{value} does not exist."
   end
 
   def print_board
-    squares.each { |square| p square }
+    @squares.each { |square| p square }
+  end
+
+  private
+
+  def create_squares
+    coordinates = []
+    [*0..7]
+      .repeated_permutation(2)
+      .to_a
+      .each { |arr| coordinates.push(Square.new(arr)) }
+    coordinates
   end
 end
